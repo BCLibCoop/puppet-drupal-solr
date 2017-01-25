ba48d8a367c397d23645753924ab15996d4687e7
Howard Tyson <howard@howardtyson.com>
2017-01-25 15:33:44 -0500

    Updating drupal_solr module to use package cloud packages.

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,76 +7,11 @@ class drupal_solr (
   $solr_drupal_7_sapi_ref = '80e7f0e343f47364ee707dc10ef3b8994784bd7c'
 ) {
 
-  package { 'tomcat6':
-    ensure => 'installed'
-  }
+  include java
+  include packagecloud
 
-  package { 'tomcat6-admin':
-    ensure => 'installed'
-  }
-
-  if ($manage_service) {
-    service { 'tomcat6':
-      require => Package['tomcat6'],
-      ensure  => 'running',
-    }
-  }
-
-  logrotate::rule { 'tomcat':
-    path         => '/var/log/tomcat/catalina.out',
-    copytruncate => true,
-    rotate_every => 'day',
-    rotate       => 5,
-    compress     => true,
-    missingok    => true,
-    size         => '10M'
-  }
-
-  file { '/opt/solr':
-    require => Package['tomcat6'],
-    ensure  => 'directory',
-    owner   => $user,
-    group   => $group,
-    mode    => '0775',
-  }
-
-  vcsrepo { '/opt/solr/solrbase-6':
-    ensure   => present,
-    provider => 'git',
-    source   => 'https://github.com/zivtech/Solr-base.git',
-    revision => $solr_drupal_6_ref,
-    owner    => $user,
-    group    => $group,
-    require  => [
-      File['/opt/solr'],
-      Package['tomcat6'],
-    ],
-  }
-
-  vcsrepo { '/opt/solr/solrbase-7':
-    ensure   => 'present',
-    provider => 'git',
-    source   => 'https://github.com/zivtech/Solr-base.git',
-    revision => $solr_drupal_7_ref,
-    owner    => $user,
-    group    => $group,
-    require  => [
-      File['/opt/solr'],
-      Package['tomcat6']
-    ],
-  }
-
-  vcsrepo { '/opt/solr/sapi-solrbase-7':
-    source   => 'https://github.com/zivtech/Solr-base.git',
-    ensure   => 'present',
-    provider => 'git',
-    revision => $solr_drupal_7_sapi_ref,
-    owner    => $user,
-    group    => $group,
-    require  => [
-      File['/opt/solr'],
-      Package['tomcat6']
-    ],
+  packagecloud::repo { "zivtech/solr":
+      type => 'deb',
   }
 
   file { '/usr/local/bin/create-solr-instance':
@@ -93,20 +28,6 @@ class drupal_solr (
     mode   => '0755',
   }
 
-  file { '/etc/tomcat6/Catalina/localhost':
-    ensure  => 'directory',
-    owner   => 'root',
-    group   => $webadmingroup,
-    require => [
-      Package['tomcat6'],
-    ],
-  }
-
-  file { '/var/lib/tomcat6/webapps':
-    ensure => 'directory',
-    owner  => $tomcatuser,
-    group  => $webadmingroup,
-  }
 }
 
 
